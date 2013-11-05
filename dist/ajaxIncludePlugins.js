cb465e0e474ee0490c3c6d94c9790aea46137227
jayseeg <jake_gibbons@the1stmovement.com>
2013-11-05 13:00:16 -0700

    added ajaxIncluded event

--- dist/ajaxIncludePlugins.js
+++ dist/ajaxIncludePlugins.js
@@ -1,4 +1,4 @@
-/*! Ajax-Include - v0.1.2 - 2013-11-04
+/*! Ajax-Include - v0.1.2 - 2013-11-05
 * http://filamentgroup.com/lab/ajax_includes_modular_content/
 * Copyright (c) 2013 @scottjehl, Filament Group, Inc.; Licensed MIT */
 
